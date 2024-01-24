2806cbb49e7848b747844d4200a33e8e9ba4a965
Andrew L Perkins <andrewlperkins@Andrews-MacBook-Pro.local>
2024-01-24 14:42:25 -0800

    first commit

---
+++ Code History/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Code History
+//
+//  Created by Andrew L Perkins on 11/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    
+    var body: some View {
+        ZStack {
+            mainColor.ignoresSafeArea()
+            
+            VStack {
+                Text("1 / 10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text("What was the first computer bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+            }
+            .foregroundColor(.white)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
