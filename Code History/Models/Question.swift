999ce7c038a91dfdf881eba3d2c9136e81eaa007
Andrew L Perkins <andrewlperkins@Andrews-MacBook-Pro.local>
2024-01-29 09:51:50 -0800

    Moved code from question to game model that was incorrect

--- Code History/Models/Question.swift
+++ Code History/Models/Question.swift
@@ -12,11 +12,6 @@ struct Question: Hashable {
     let possibleAnswers: [String]
     let correctAnswerIndex: Int
     
-    private(set) var isOver = false
-    private(set) var currentQuestionIndex = 0
-    private(set) var guesses = [Question: Int]()
-    private let questions = Question.allQuestions.shuffled()
-    
     static var allQuestions = [
         Question(questionText: "Who invented the World Wide Web?",
                  possibleAnswers: [
@@ -35,36 +30,4 @@ struct Question: Hashable {
                  ],
                  correctAnswerIndex: 0)
     ]
-    
-    var guessCount: (correct: Int, incorrect: Int) {
-        var count: (correct: Int, incorrect: Int) = (0, 0)
-        for (question, guessedIndex) in guesses {
-            if question.correctAnswerIndex == guessedIndex {
-                count.correct += 1
-            } else {
-                count.incorrect += 1
-            }
-        }
-        return count
-    }
-    
-    var numberOfQuestions: Int {
-        questions.count
-    }
-    
-    var currentQuestion: Question {
-        questions[currentQuestionIndex]
-    }
-    
-    mutating func makeGuessForCurrentQuestion(atIndex index: Int) {
-        guesses[currentQuestion] = index
-    }
-    
-    mutating func updateGameStatus() {
-        if currentQuestionIndex < questions.count - 1 {
-            currentQuestionIndex += 1
-        } else {
-            isOver = true
-        }
-    }
 }
